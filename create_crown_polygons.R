4e4fe211292d5c085973e41661302479fef61d1a
vscholl <victoria.scholl@colorado.edu>
2018-08-14 10:27:32 -0600

    Started code to create crown polygons using NS/EW radius, diam, azimuth

---
+++ create_crown_polygons.R
@@ -0,0 +1,14 @@
+# calculate coordinates for tree polygon points 
+
+# center point 
+x <- 5 
+y <- 10 
+center <- c(x, y)
+
+# NS measurements
+ns_rad <- 1.1
+ns_diam <- 2.9
+
+# Azimuth angle of 
+az <- 40
+
